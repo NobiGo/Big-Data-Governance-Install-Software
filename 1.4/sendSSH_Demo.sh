a138cd785560dd1bccf9863262c7995aa62c0710
NobiGo <nobigogle@gmail.com>
2017-01-05 15:02:54 +0800

    成功配置集群Root用户的SSH稳定版

---
+++ 1.4/sendSSH_Demo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HostFile=/root/hosts
+cat $HostFile | while read line
+do
+ip=`echo "$line" | awk '{print $1}'` #输出每行ip字段
+username=`echo "$line" | awk '{print $2}'` #输出每行usename字段
+passwd=`echo "$line" | awk '{print $3}'` #输出每行passwd字段
+hostname=`echo "$line" | awk '{print $4}'`
+
+#脚本生成ssh公私钥文件
+/usr/bin/expect << EOF
+spawn   ssh-copy-id $username@$hostname
+expect {
+"*yes/no*" {send "yes\r" ;exp_continue}
+"*password:" {send "123456\r";exp_continue}
+}
+EOF
+done
